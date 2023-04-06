01eae295b0690d1b37c915d11441a488bab9e250
yan <writetoyan@protonmail.com>
2023-04-06 18:09:37 +0200

    update

--- frontend/services/footballApi.js
+++ frontend/services/footballApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const footballHeaders = {
-    'X-RapidAPI-Key': 'fa886cecfcmsh8bed26a3b845cb8p1a4d0ejsnef034b5bbb1e',
+    'X-RapidAPI-Key': '',
     'X-RapidAPI-Host': 'footapi7.p.rapidapi.com'
 };
 const baseUrl = 'https://footapi7.p.rapidapi.com';
